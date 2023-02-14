9b5a90d5926e8d2109dd7fbca2d86a6c2d810b04
Moamin <moamin.salamah@gmail.com>
2023-02-14 09:18:48 +0100

    project setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HeaderPart />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import HeaderPart from "@/components/HeaderPart.vue";
+
+export default {
+  components: {
+    HeaderPart,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/HeaderPart.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <div class="nav-inner">
+      <!-- logo -->
+      <div class="logo">
+        <img src="@/assets/images/logo.png" alt="logo" />
+      </div>
+      <!-- links -->
+      <!-- icons -->
+    </div>
+  </nav>
+</template>
+
+<script></script>
+
+<style></style>
